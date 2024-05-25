15f2ce57f06963deccfc27db580811668bcb9ec3
My Name <dlm1337.dm@gmail.com>
2024-05-25 18:48:05 -0400

    working migration for tables

---
+++ Expert-Freezer-WebAPI/Migrations/20240525224349_InitialCreate.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace ExpertFreezerWebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "users",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Username = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    Email = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_users", x => x.Id);
+                    table.UniqueConstraint("AK_users_Username", x => x.Username);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "expertFreezerProfiles",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Username = table.Column<string>(type: "text", nullable: true),
+                    CompanyName = table.Column<string>(type: "text", nullable: true),
+                    ProfilePic = table.Column<string>(type: "text", nullable: true),
+                    ExtraPics = table.Column<List<string>>(type: "text[]", nullable: true),
+                    ExtraPicsDesc = table.Column<List<string>>(type: "text[]", nullable: true),
+                    CompanyDescription = table.Column<string>(type: "text", nullable: true),
+                    Services = table.Column<string>(type: "text", nullable: true),
+                    Address = table.Column<string>(type: "text", nullable: true),
+                    Email = table.Column<string>(type: "text", nullable: true),
+                    Pricing = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_expertFreezerProfiles", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_expertFreezerProfiles_users_Username",
+                        column: x => x.Username,
+                        principalTable: "users",
+                        principalColumn: "Username",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_expertFreezerProfiles_Username",
+                table: "expertFreezerProfiles",
+                column: "Username",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_users_Username",
+                table: "users",
+                column: "Username",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "expertFreezerProfiles");
+
+            migrationBuilder.DropTable(
+                name: "users");
+        }
+    }
+}
